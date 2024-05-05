d3cbd5783f44c6a535f08388350d74a8e5a05969
Cesar-Zanellato <cesar_zanellato@estudante.sesisenai.org.br>
2024-05-05 17:37:51 -0300

    Atualizando o cabecalho

---
+++ fortesupermercados/src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <section className="section_logo">
+        <image>
+          <img src="./public/logo_supermercado.png" alt="FORTESUPEMERCADO Logo" />
+        </image>
+      </section>
+      <section className="section_input">
+        <input className="input_pesquisar" type="text" placeholder="Pesquisar produtos..." />
+      </section>
+      <section className="section_btns_user">
+          <img src="./public/carrinho_compras.png" alt="CARRINHO"/>
+          <img src="./public/icone_usuario.png" alt="USUÁRIO"/>
+      </section>
+    </header>
+  );
+}
+
+export default Header;
