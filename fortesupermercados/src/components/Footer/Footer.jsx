4e20333681c21a147e1b76e634d00bd5e649aeca
LUIGI DOS SANTOS BERNARDO <luigi_bernardo@estudante.sesisenai.org.br>
2024-05-16 21:46:15 -0300

    Atualizando conteudo Front

--- fortesupermercados/src/components/Footer/Footer.jsx
+++ fortesupermercados/src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
 
       <div className="titulos">
         <h2 className='tituloHorario'>Horário de Atendimento</h2>
-        <h2 className='tituloLocalização'>Localização</h2>
+        <h2 className='tituloLocalização'>Fale Conosco</h2>
         <h2 className='tituloEndereço'>Endereço</h2> 
       </div>
 
@@ -20,8 +20,61 @@ function Footer() {
         <h2 className='subDelivery'>Delivery</h2>
       </div>
 
-      <div className='divTextos'>
-        
+      <div className='divTextos'> 
+
+        <div className='divHoraMercado'>
+
+          <div className='divSegSab'>
+          <img className='relogio' src="./public/relogio.png" alt="Relegio" />
+            <p className='textSegSab'>Segunda a Sábado | 07:00 até 23:00 </p>
+          </div>
+
+          <div className='divDom'>
+            <img className='relogio' src="./public/relogio.png" alt="Relegio" />
+            <p className='textSegSab'>Domingo | 08:00 até 23:00  </p>
+          </div>
+
+        </div>
+
+        <div className='divHoraDelivery'>
+
+        <div className='divSegSab'>
+          <img className='relogio' src="./public/relogio.png" alt="Relegio" />
+            <p className='textSegSab'>Segunda a Sábado | 08:00 até 22:00  </p>
+          </div>
+
+          <div className='divDom'>
+            <img className='relogio' src="./public/relogio.png" alt="Relegio" />
+            <p className='textSegSab'>Domingo | 09:00 até 21:00  </p>
+          </div>
+
+        </div>
+
+        <div className='divFaleConosco'>
+          <div className='divSegSab'>
+          <img className='telefone' src="./public/telefone.png" alt="Relegio" />
+            <p className='textSegSab'>(48) 3284-5827 Matriz </p>
+          </div>
+
+          <div className='divDom'>
+            <img className='email' src="./public/email.png" alt="Relegio" />
+            <p className='textSegSab'>contato@fortesupermercados.com.br  </p>
+          </div>
+        </div>
+
+        <div className='divEndereço'>
+        <div className='divTextEndereço'>
+          <img className='telefone' src="./public/telefone.png" alt="Relegio" />
+            <p className='textSegSab'>Rua Leonel Pereira, 99 Cachoeira do Bom Jesus,
+CEP-88056-300, Florianópolis - Santa Catarina - Brasil </p>
+          </div>
+
+          <div className='divDom'>
+            <img className='email' src="./public/email.png" alt="Relegio" />
+            <p className='textSegSab'>contato@fortesupermercados.com.br  </p>
+          </div>
+        </div>
+
       </div>
 
     </footer>
