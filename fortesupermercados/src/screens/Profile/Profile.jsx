43d35219c1c6ffd8cc121abfb7f7c9c1a5d7b6db
cirojuniorvg <115041894+cirojuniorvg@users.noreply.github.com>
2024-06-05 18:20:28 -0300

    profile no shop

--- fortesupermercados/src/screens/Profile/Profile.jsx
+++ fortesupermercados/src/screens/Profile/Profile.jsx
@@ -1,13 +1,72 @@
-import React from 'react'
+import React from 'react';
+import './Profile.css';
 
-function Profile() {
+const Profile = () => {
   return (
-    <div>
-        <h1>
-            EU SOU O PERFIL
-        </h1>
-    </div>
-  )
-}
+    <div className="profile-container">
+      <div className="personal-info">
+        <div className="column">
+          <div className="info-item">
+            <label>Nome:</label>
+            <input type="text" />
+          </div>
+          <div className="info-item">
+            <label>CPF:</label>
+            <input type="text" />
+          </div>
+          <div className="info-item">
+            <label>Senha:</label>
+            <input type="password" />
+          </div>
+        </div>
+        <div className="column">
+          <div className="info-item">
+            <label>Email:</label>
+            <input type="text" />
+          </div>
+          <div className="info-item">
+            <label>Telefone:</label>
+            <input type="text" />
+          </div>
+          <div className="info-item">
+            <label>Data de Nascimento:</label>
+            <input type="date" />
+          </div>
+        </div>
+        <div className="buttons">
+          <button className="edit-button">Editar</button>
+          <button className="save-button">Salvar</button>
+        </div>
+      </div>
 
-export default Profile
+      <div className="address-info">
+        <div className="info-item">
+          <label>Nome da Rua:</label>
+          <input type="text" />
+        </div>
+        <div className="info-item">
+          <label>Número:</label>
+          <input type="text" />
+        </div>
+        <div className="info-item">
+          <label>CEP:</label>
+          <input type="text" />
+        </div>
+        <div className="info-item">
+          <label>Ponto de Referência:</label>
+          <input type="text" />
+        </div>
+        <div className="info-item">
+          <label>Complemento:</label>
+          <input type="text" />
+        </div>
+        <div className="buttons">
+          <button className="edit-button">Editar</button>
+          <button className="save-button">Salvar</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
--- fortesupermercados/src/screens/Shop/Shop.jsx
+++ fortesupermercados/src/screens/Shop/Shop.jsx
@@ -1,13 +1,72 @@
-import React from 'react'
+import React from 'react';
+import './Shop.css';
 
-function Shop() {
+const Shop = () => {
   return (
-    <div>
-      <h1>
-        EU SOU O SHOP
-      </h1>
-    </div>
-  )
-}
+    <div className="profile-container">
+      <div className="personal-info">
+        <div className="column">
+          <div className="info-item">
+            <label>Nome:</label>
+            <input type="text" />
+          </div>
+          <div className="info-item">
+            <label>CPF:</label>
+            <input type="text" />
+          </div>
+          <div className="info-item">
+            <label>Senha:</label>
+            <input type="password" />
+          </div>
+        </div>
+        <div className="column">
+          <div className="info-item">
+            <label>Email:</label>
+            <input type="text" />
+          </div>
+          <div className="info-item">
+            <label>Telefone:</label>
+            <input type="text" />
+          </div>
+          <div className="info-item">
+            <label>Data de Nascimento:</label>
+            <input type="date" />
+          </div>
+        </div>
+        <div className="buttons">
+          <button className="edit-button">Editar</button>
+          <button className="save-button">Salvar</button>
+        </div>
+      </div>
 
-export default Shop
+      <div className="address-info">
+        <div className="info-item">
+          <label>Nome da Rua:</label>
+          <input type="text" />
+        </div>
+        <div className="info-item">
+          <label>Número:</label>
+          <input type="text" />
+        </div>
+        <div className="info-item">
+          <label>CEP:</label>
+          <input type="text" />
+        </div>
+        <div className="info-item">
+          <label>Ponto de Referência:</label>
+          <input type="text" />
+        </div>
+        <div className="info-item">
+          <label>Complemento:</label>
+          <input type="text" />
+        </div>
+        <div className="buttons">
+          <button className="edit-button">Editar</button>
+          <button className="save-button">Salvar</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
