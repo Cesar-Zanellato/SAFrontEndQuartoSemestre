eaba42b3a4ca5290f8f0d5b50210b8eab5a81ee0
LUIGI DOS SANTOS BERNARDO <luigi_bernardo@estudante.sesisenai.org.br>
2024-06-17 21:46:09 -0300

    Atualizando Front

--- fortesupermercados/src/screens/Home/Home.jsx
+++ fortesupermercados/src/screens/Home/Home.jsx
@@ -1,12 +1,82 @@
 import React from 'react'
-
+import './Home.css'
 function Home() {
+
+
   return (
-    <div>
-      <h1>
-        EU SOU A HOME
-      </h1>
-    </div>
+    <>
+      <div className='divCarrossel'>
+
+        <img src="carrossel.png" alt="" />
+
+      </div>
+
+      <h1 className='textMaisVendidos'>Mais Vendidos</h1>
+
+      <div className='divForaMaisVendidos'>
+        
+        <div className='divDentroMaisVendidos'>
+
+          <div className='divProduto'>
+
+              <div className='divImgProduto'>
+
+                  <img className='imgCocaCola' src="coca_cola.png" alt="" />
+
+              </div>
+
+              <div className='divTituloProduto'>
+
+                <p className='tituloProduto'>Refrigerante Coca-Cola 2L</p>
+
+              </div>
+
+              <div className='divCompraProduto'>
+                <p className='textValor'>9,99</p>
+                  <div className='divQuantidade'>
+
+                    <div className='botaoMais'>
+
+                    </div>
+
+                    <div className='botaoMenos'>
+                      
+                    </div>
+
+                  </div>
+
+                  <div className='divAdicionar'>
+                    <p className='textAdicionar'>Adicionar</p>
+                  </div>
+              </div>
+            
+          </div>
+
+          <div className='divProduto'>
+
+          </div>
+
+          <div className='divProduto'>
+
+          </div>
+
+          <div className='divProduto'>
+
+          </div>
+
+          <div className='divProduto'>
+
+          </div>
+
+          
+
+        </div>
+
+      </div>
+    </>
+
+
+    
   )
 }
 
